34445feb3dd5d570aa4cd7df6ad3152b424388a2
Antonio Dovao <adovao@aidacanarias.com>
2024-06-11 10:01:41 +0100

    ✨ Added Program.Main

---
+++ hello-service/HelloService/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+
+namespace Hello
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var helloService = new HelloService(new SystemDateTimeProvider(), new ConsoleOutput());
+
+            helloService.Hello();
+        }
+    }
+}
