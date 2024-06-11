74520aebf6f93532cb2390b7c854e0e9e853c2db
Antonio Dovao <adovao@aidacanarias.com>
2024-06-11 09:20:48 +0100

    ✅ say_good_morning_at_six_o_clock

--- hello-service/HelloService.Tests/HelloServiceTest.cs
+++ hello-service/HelloService.Tests/HelloServiceTest.cs
@@ -1,3 +1,4 @@
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Hello.Tests
@@ -5,9 +6,43 @@ namespace Hello.Tests
     public class HelloServiceTest
     {
         [Test]
-        public void Canary_Test()
+        public void say_good_morning_at_six_o_clock()
         {
-            Assert.That(true, Is.True);
+            var dateTimeProvider = Substitute.For<DateTimeProvider>();
+            var output = Substitute.For<Output>();
+            dateTimeProvider.GetDateTime().Returns(DateTime.Parse("01/02/2024 06:00:00"));
+            var helloService = new HelloService(dateTimeProvider, output);
+
+            helloService.Hello();
+
+            output.Received(1).Send("Buenos días!");
+        }
+    }
+
+    public interface Output
+    {
+        void Send(string message);
+    }
+
+    public interface DateTimeProvider
+    {
+        DateTime GetDateTime();
+    }
+
+    public class HelloService
+    {
+        private readonly DateTimeProvider _dateTimeProvider;
+        private readonly Output _output;
+
+        public HelloService(DateTimeProvider dateTimeProvider, Output output)
+        {
+            _dateTimeProvider = dateTimeProvider;
+            _output = output;
+        }
+
+        public void Hello()
+        {
+            _output.Send("Buenos días!");
         }
     }
 }
