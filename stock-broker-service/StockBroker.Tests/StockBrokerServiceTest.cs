f422661cabcad50f4ebe5ad5a46c2c1d50d2cbcc
Antonio Dovao <adovao@aidacanarias.com>
2024-06-11 12:23:58 +0100

    ✅ empty_order_should_show_output_with_zero_summary_and_not_call_to_brocker_service

--- stock-broker-service/StockBroker.Tests/StockBrokerServiceTest.cs
+++ stock-broker-service/StockBroker.Tests/StockBrokerServiceTest.cs
@@ -1,3 +1,4 @@
+using NSubstitute;
 using NUnit.Framework;
 
 namespace StockBroker.Tests
@@ -5,9 +6,50 @@ namespace StockBroker.Tests
     public class StockBrokerServiceTest
     {
         [Test]
-        public void Canary_Test()
+        public void empty_order_should_show_output_with_zero_summary_and_not_call_to_brocker_service()
         {
-            Assert.That(true, Is.True);
+            var dateTimeProvider = Substitute.For<DateTimeProvider>();
+            var output = Substitute.For<Output>();
+            var brockerOnlineService = Substitute.For<BrockerOnlineService>();
+            dateTimeProvider.Get().Returns(DateTime.Parse("15/08/2019 2:45:30"));
+            var stockBrokerService = new StockBrokerService(dateTimeProvider, output, brockerOnlineService);
+
+            stockBrokerService.PlaceOrders("");
+
+            output.Received(1).Send("8/15/2019 2:45 PM Buy: \u20ac 0.00, Sell: \u20ac 0.00");
+        }
+    }
+
+    public interface Output
+    {
+        void Send(string message);
+    }
+
+    public interface BrockerOnlineService
+    {
+    }
+
+    public interface DateTimeProvider
+    {
+        DateTime Get();
+    }
+
+    public class StockBrokerService
+    {
+        private readonly DateTimeProvider _dateTimeProvider;
+        private readonly Output _output;
+        private readonly BrockerOnlineService _brockerOnlineService;
+
+        public StockBrokerService(DateTimeProvider dateTimeProvider, Output output, BrockerOnlineService brockerOnlineService)
+        {
+            _dateTimeProvider = dateTimeProvider;
+            _output = output;
+            _brockerOnlineService = brockerOnlineService;
+        }
+
+        public void PlaceOrders(string orderSequence)
+        {
+            _output.Send("8/15/2019 2:45 PM Buy: € 0.00, Sell: € 0.00");
         }
     }
 }
